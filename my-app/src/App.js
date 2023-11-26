08249123c08f5e97b33d5aeeb59d1bcd58514c4f
RIO-20 <ionescu.raul04@gmail.com>
2023-11-26 22:00:39 +0200

    prima parte

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyNavbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar/>
+      
+    </div>
+  );
+}
+
+export default App;
+
